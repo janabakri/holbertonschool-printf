a1b5aea4558b6c5ce4a14168161516ab3f8a9322
janabakri <bkeyh382@gmail.com>
2025-11-17 09:52:56 +0000

    Implement simplified _printf in C supporting %c, %s, and %%
    - Parses the format string and identifies format specifiers.
    - Prints characters and strings using _putchar.
    - Handles NULL strings safely by printing (null).
    - Returns the total number of printed characters, excluding the null byte.

--- functions.c
+++ functions.c
@@ -1,64 +1,49 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string to stdout
- * @s: The string to print
- *
- * Return: The number of characters printed
- */
-int print_string(char *s)
-{
-	int count = 0;
-
-	if (!s)
-		s = "(null)";
-
-	while (*s)
-	{
-		count += _putchar(*s);
-		s++;
-	}
-
-	return (count);
-}
-
-/**
- * print_number - prints an integer to stdout
- * @n: The integer to print
- *
- * Return: The number of characters printed
+ * print_number - Prints an integer using _putchar
+ * @n: Integer to print
+ * Return: Number of characters printed
  */
 int print_number(int n)
 {
-	int count = 0;
-	unsigned int num;
+    unsigned int num;
+    int count = 0;
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
+    if (n < 0)
+    {
+        count += _putchar('-');
+        num = -n;
+    }
+    else
+        num = n;
 
-	if (num / 10)
-		count += print_number(num / 10);
+    if (num / 10)
+        count += print_number(num / 10);
 
-	count += _putchar((num % 10) + '0');
+    count += _putchar((num % 10) + '0');
 
-	return (count);
+    return (count);
+}
+
+/**
+ * print_string - Prints a string using _putchar
+ * @s: String to print
+ * Return: Number of characters printed
+ */
+int print_string(char *s)
+{
+    int count = 0;
+
+    if (!s)
+        s = "(null)";
+
+    while (*s)
+    {
+        count += _putchar(*s);
+        s++;
+    }
+    return (count);
 }
 
