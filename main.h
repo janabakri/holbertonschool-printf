ff53f87401c9f3df22924005f1af26c3f5759fee
janabakri <bkeyh382@gmail.com>
2025-11-17 09:14:42 +0000

    Implemented a simplified _printf function in C supporting %c, %s, and %%. It parses the format string, prints characters or strings using _putchar, handles NULL strings safely, and returns the total number of printed characters.

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* _printf function prototype */
+int _printf(const char *format, ...);
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+#endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,18 +1,28 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include "main.h"
 
-/* Helper function to print a single character */
+/**
+ * _putchar - writes a single character to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success
+ */
 int _putchar(char c)
 {
     return write(1, &c, 1);
 }
 
-/* _printf: simplified printf supporting %c, %s, %% */
+/**
+ * _printf - prints formatted output to stdout
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
     va_list args;
     int i = 0, count = 0;
     char *str;
+    char c;
 
     if (!format)
         return (-1);
@@ -25,31 +35,29 @@ int _printf(const char *format, ...)
         {
             i++;
             if (!format[i])
-                break;
+                return (-1); /* % at end of string */
 
-            switch (format[i])
+            if (format[i] == 'c') /* Character */
             {
-                case 'c':
-                    count += _putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (!str)
-                        str = "(null)";
-                    while (*str)
-                    {
-                        count += _putchar(*str);
-                        str++;
-                    }
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                default:
-                    /* Unknown specifier: print % and the character */
-                    count += _putchar('%');
-                    count += _putchar(format[i]);
-                    break;
+                c = (char)va_arg(args, int);
+                count += _putchar(c);
+            }
+            else if (format[i] == 's') /* String */
+            {
+                str = va_arg(args, char *);
+                if (!str)
+                    str = "(null)";
+                while (*str)
+                    count += _putchar(*str++);
+            }
+            else if (format[i] == '%') /* Percent sign */
+            {
+                count += _putchar('%');
+            }
+            else /* Unknown specifier */
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
             }
         }
         else
@@ -61,32 +69,5 @@ int _printf(const char *format, ...)
 
     va_end(args);
     return count;
-}
-
-/* Example main to test all cases */
-int main(void)
-{
-    char *str = "This is a big test string for _printf function to check large output handling.\n";
-
-    _printf("Simple sentence:\nLet's print a simple sentence.\n");
-    _printf("%c\n", 'S');
-    _printf("A char inside a sentence: %c. Did it work?\n", 'F');
-    _printf("Let's check the cast: %c. Did it work?\n", 48);
-    _printf("%s", "This sentence is retrieved from va_args!\n");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-    _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c",
-            'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-    _printf("Percent signs: %%\n");
-    _printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-    _printf("css%ccs%scscscs\n", 'T', "Test");
-    _printf("%s", str);
-    _printf("man gcc:\n%s", str);
-    _printf("%s\n", NULL);
-    _printf("%c\n", '\0');
-    _printf("Unknown specifiers: %!\n");
-    _printf("Another unknown: %K\n");
-
-    return 0;
 }
 
