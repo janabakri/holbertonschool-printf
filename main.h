b2f8d2ee781468d2020c8fd5db5e657687e9fbb1
janabakri <bkeyh382@gmail.com>
2025-11-17 10:48:42 +0000

    feat: implement basic _printf (Task 0)
    - Supports %c, %s, %%
    - Uses write() and returns number of characters printed
    - Handles NULL strings as '(null)' and follows Betty style

--- main.h
+++ main.h
@@ -1,21 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-
-/* _printf function */
 int _printf(const char *format, ...);
-
-/* Character printing function */
-int _putchar(char c);
-
-/* String printing function */
-int print_string(char *s);
-
-/* Integer printing function */
-int print_number(int n);
 
 #endif /* MAIN_H */
 
--- printf.c
+++ printf.c
@@ -1,115 +1,66 @@
-#include "main.h"
-#include <stdarg.h>
+                                                                                                                                                                                                                                    #include <stdarg.h>
 #include <unistd.h>
-
-/**
- * _putchar - writes a character to stdout
- * @c: character to print
- * Return: 1 on success
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string
- * @s: string to print
- * Return: number of characters printed
- */
-int print_string(char *s)
-{
-    int count = 0;
-
-    if (!s)
-        s = "(null)";
-
-    while (*s)
-    {
-        count += _putchar(*s);
-        s++;
-    }
-    return (count);
-}
-
-/**
- * print_number - prints an integer
- * @n: number to print
- * Return: number of characters printed
- */
-int print_number(long int n)
-{
-    unsigned long int num;
-    int count = 0;
-
-    if (n < 0)
-    {
-        count += _putchar('-');
-        num = -n;
-    }
-    else
-        num = n;
-
-    if (num / 10)
-        count += print_number(num / 10);
-
-    count += _putchar((num % 10) + '0');
-    return (count);
-}
+#include "main.h"
 
 /**
  * _printf - produces output according to a format
- * @format: format string containing format specifiers
- * Return: number of characters printed (excluding null byte)
+ * @format: format string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
+    const char *p;
 
     if (!format)
         return (-1);
 
     va_start(args, format);
 
-    while (*format)
+    for (p = format; *p; p++)
     {
-        if (*format == '%')
+        if (*p == '%')
         {
-            format++;
-            if (!*format)
-                return (-1);
-
-            switch (*format)
+            p++;
+            if (*p == 'c')
             {
-                case 'c':
-                    count += _putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    count += print_string(va_arg(args, char *));
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                case 'd':
-                case 'i':
-                    count += print_number(va_arg(args, int));
-                    break;
-                default:
-                    count += _putchar('%');
-                    count += _putchar(*format);
-                    break;
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*p == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (!s)
+                    s = "(null)";
+                while (*s)
+                {
+                    write(1, s, 1);
+                    s++;
+                    count++;
+                }
+            }
+            else if (*p == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else
+            {
+                write(1, "%", 1);
+                write(1, p, 1);
+                count += 2;
             }
         }
         else
         {
-            count += _putchar(*format);
+            write(1, p, 1);
+            count++;
         }
-        format++;
     }
 
     va_end(args);
     return (count);
 }
-
 
