9310a43077d17b3b681f539238b7fa5df65fc6b9
janabakri <bkeyh382@gmail.com>
2025-11-17 09:36:31 +0000

    Implemented a simplified _printf function in C supporting %c, %s, and %%. It parses the format string, prints characters or strings using _putchar, handles NULL strings safely, and returns the total number of printed characters.

--- printf.c
+++ printf.c
@@ -1,99 +1,43 @@
-#include "main.h"
-
-/**
- * _putchar - writes a single character to stdout
- * @c: character to print
- *
- * Return: 1 on success
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
- * _printf - prints formatted output to stdout
- * @format: format string
- *
- * Return: number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
-	char *str;
-	char c;
+    va_list args;
+    int count = 0;
 
-	if (!format)
-		return (-1);
+    if (!format || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!format[i])
-				return (-1);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+                return (-1);
 
-			switch (format[i])
-			{
-				case 'c':
-					c = (char)va_arg(args, int);
-					count += _putchar(c);
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (!str)
-						str = "(null)";
-					while (*str)
-						count += _putchar(*str++);
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				case 'd':
-				case 'i':
-					count += print_number(args, 10, 0, 0);
-					break;
-				case 'u':
-					count += print_number(args, 10, 1, 0);
-					break;
-				case 'o':
-					count += print_number(args, 8, 1, 0);
-					break;
-				case 'x':
-					count += print_number(args, 16, 1, 0);
-					break;
-				case 'X':
-					count += print_number(args, 16, 1, 1);
-					break;
-				case 'b':
-					count += print_binary(args);
-					break;
-				case 'S':
-					count += print_S(args);
-					break;
-				case 'r':
-					count += print_reverse(args);
-					break;
-				case 'R':
-					count += print_rot13(args);
-					break;
-				default:
-					count += _putchar('%');
-					count += _putchar(format[i]);
-			}
-		}
-		else
-		{
-			count += _putchar(format[i]);
-		}
-		i++;
-	}
+            if (*format == 'c')
+                count += _putchar(va_arg(args, int));
+            else if (*format == 's')
+                count += print_string(va_arg(args, char *));
+            else if (*format == '%')
+                count += _putchar('%');
+            else if (*format == 'd' || *format == 'i')
+                count += print_number(va_arg(args, int));
+            else
+                count += _putchar('%');  // لطباعة % إذا كان specifier غير معروف
 
-	va_end(args);
-	return (count);
+            format++;  // زيادة المؤشر بعد التعامل مع specifier
+        }
+        else
+        {
+            count += _putchar(*format);
+            format++;
+        }
+    }
+
+    va_end(args);
+    return (count);
 }
+
 
