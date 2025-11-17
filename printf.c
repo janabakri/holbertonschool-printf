ab5a708a08593d998dc72762773f4319ea2e27f3
janabakri <bkeyh382@gmail.com>
2025-11-17 14:11:57 +0000

    Supports %c, %s, %% - Uses write() and returns the number of characters printed - Handles NULL strings as "(null)"

--- printf.c
+++ printf.c
@@ -1,105 +1,68 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* ---- Helpers ---- */
-
-int write_char(char c)
-{
-    return (write(1, &c, 1));
-}
-
-int print_char(va_list args)
-{
-    char c = va_arg(args, int);
-    return (write_char(c));
-}
-
-int print_string(va_list args)
-{
-    char *s = va_arg(args, char *);
-    int i = 0;
-
-    if (!s)
-        s = "(null)";
-
-    while (s[i])
-    {
-        write_char(s[i]);
-        i++;
-    }
-    return (i);
-}
-
-int print_percent(void)
-{
-    return (write(1, "%", 1));
-}
-
-/* ---- Core format processing ---- */
-
-int handle_specifier(const char *format, int *i, va_list args)
-{
-    int count = 0;
-
-    (*i)++;
-    if (format[*i] == '\0')
-        return (-1);
-
-    if (format[*i] == 'c')
-        count = print_char(args);
-    else if (format[*i] == 's')
-        count = print_string(args);
-    else if (format[*i] == '%')
-        count = print_percent();
-    else
-    {
-        write(1, "%", 1);
-        write(1, &format[*i], 1);
-        count = 2;
-    }
-
-    return (count);
-}
-
-int process_format(const char *format, va_list args)
-{
-    int count = 0, i = 0, add;
-
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            add = handle_specifier(format, &i, args);
-            if (add == -1)
-                return (-1);
-            count += add;
-        }
-        else
-            count += write_char(format[i]);
-        i++;
-    }
-
-    return (count);
-}
-
-/* ---- MAIN _printf FUNCTION ---- */
-
+/**
+ * _printf - prints according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed, or -1 on error
+ */
 int _printf(const char *format, ...)
 {
     va_list args;
-    int printed;
+    int count = 0, i = 0;
 
     if (format == NULL)
         return (-1);
 
     va_start(args, format);
-    printed = process_format(format, args);
-    va_end(args);
 
-    return (printed);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                count += write(1, &c, 1);
+            }
+            else if (format[i] == 's')
+            {
+                char *s = va_arg(args, char *);
+                int j = 0;
+                if (!s)
+                    s = "(null)";
+                while (s[j])
+                {
+                    write(1, &s[j], 1);
+                    j++;
+                }
+                count += j;
+            }
+            else if (format[i] == '%')
+            {
+                count += write(1, "%", 1);
+            }
+            else
+            {
+                count += write(1, "%", 1);
+                count += write(1, &format[i], 1);
+            }
+        }
+        else
+        {
+            count += write(1, &format[i], 1);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
 }
 
 /* ---- Example main to test ---- */
+#ifdef TEST_PRINTF
 int main(void)
 {
     _printf("Hello %corld%s %%\n", 'W', "!");
@@ -107,4 +70,5 @@ int main(void)
     _printf("Just a percent sign: %%\n");
     return (0);
 }
+#endif
 
