2d2da6a909237d264c60452bbbefb82bc542c59b
janabakri <bkeyh382@gmail.com>
2025-11-18 19:44:07 +0000

    8. Implement +, space, and # flags for _printf #advanced

--- printf.c
+++ printf.c
@@ -1,106 +1,122 @@
-#include "main.h"
-#include <stdarg.h>
+                                                                                                                                                                                                                                                                                                                                                                                                                                    #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
-#define BUFFER_SIZE 1024
-
-/* Function prototypes */
-int _printf(const char *format, ...);
-int flush_buffer(char *buffer, int *buff_index);
-int buffer_char(char c, char *buffer, int *buff_index);
-int buffer_string(char *s, char *buffer, int *buff_index);
-int buffer_pointer(void *ptr, char *buffer, int *buff_index);
-
-/* Flush buffer to stdout */
-int flush_buffer(char *buffer, int *buff_index)
+/* Helper to write a single character */
+int _putchar(char c)
 {
-    int written = write(1, buffer, *buff_index);
-    *buff_index = 0;
-    return written;
+    return write(1, &c, 1);
 }
 
-/* Add char to buffer */
-int buffer_char(char c, char *buffer, int *buff_index)
+/* Helper to print number with flags */
+int print_number(int n, int plus_flag, int space_flag)
 {
     int count = 0;
-    buffer[*buff_index] = c;
-    (*buff_index)++;
-    if (*buff_index == BUFFER_SIZE)
-        count += flush_buffer(buffer, buff_index);
-    return count;
-}
+    unsigned int num;
 
-/* Add string to buffer */
-int buffer_string(char *s, char *buffer, int *buff_index)
-{
-    int count = 0;
-    if (!s)
-        s = "(null)";
-    while (*s)
-        count += buffer_char(*s++, buffer, buff_index);
-    return count;
-}
-
-/* Add pointer address to buffer */
-int buffer_pointer(void *ptr, char *buffer, int *buff_index)
-{
-    int count = 0;
-    unsigned long addr;
-    char hex[16];
-    int i;
-
-    if (!ptr)
-        return buffer_string("(nil)", buffer, buff_index);
-
-    addr = (unsigned long)ptr;
-    buffer_char('0', buffer, buff_index);
-    buffer_char('x', buffer, buff_index);
-
-    i = 0;
-    while (addr > 0)
+    if (n < 0)
     {
-        hex[i++] = "0123456789abcdef"[addr % 16];
-        addr /= 16;
+        count += _putchar('-');
+        num = -n;
     }
-    if (i == 0)
-        hex[i++] = '0';
-    while (--i >= 0)
-        count += buffer_char(hex[i], buffer, buff_index);
+    else
+    {
+        if (plus_flag)
+            count += _putchar('+');
+        else if (space_flag)
+            count += _putchar(' ');
+        num = n;
+    }
+
+    /* Print the number */
+    if (num / 10)
+        count += print_number(num / 10, 0, 0);
+    count += _putchar((num % 10) + '0');
 
     return count;
 }
 
-/* _printf implementation */
+/* Helper to print unsigned in base with # flag */
+int print_unsigned(unsigned int n, int base, int hash_flag, int uppercase)
+{
+    int count = 0;
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (hash_flag && n != 0)
+    {
+        if (base == 8)
+            count += _putchar('0');
+        else if (base == 16)
+        {
+            count += _putchar('0');
+            count += _putchar(uppercase ? 'X' : 'x');
+        }
+    }
+
+    if (n / base)
+        count += print_unsigned(n / base, base, 0, uppercase);
+
+    count += _putchar(digits[n % base]);
+
+    return count;
+}
+
+/* Simplified _printf handling +, space, # flags */
 int _printf(const char *format, ...)
 {
     va_list args;
-    int count = 0, i = 0, buff_index = 0;
-    char buffer[BUFFER_SIZE];
-
+    int i = 0, count = 0;
     va_start(args, format);
 
     while (format[i])
     {
-        if (format[i] == '%' && format[i + 1])
+        if (format[i] == '%')
         {
+            int plus_flag = 0, space_flag = 0, hash_flag = 0;
             i++;
-            if (format[i] == 's')
-                count += buffer_string(va_arg(args, char *), buffer, &buff_index);
-            else if (format[i] == 'c')
-                count += buffer_char(va_arg(args, int), buffer, &buff_index);
-            else if (format[i] == 'p')
-                count += buffer_pointer(va_arg(args, void *), buffer, &buff_index);
-            /* Add other specifiers here (d, i, b, S, etc.) */
+
+            /* Parse flags */
+            while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+            {
+                if (format[i] == '+') plus_flag = 1;
+                else if (format[i] == ' ') space_flag = 1;
+                else if (format[i] == '#') hash_flag = 1;
+                i++;
+            }
+
+            /* Handle specifiers */
+            if (format[i] == 'd' || format[i] == 'i')
+                count += print_number(va_arg(args, int), plus_flag, space_flag);
+            else if (format[i] == 'o')
+                count += print_unsigned(va_arg(args, unsigned int), 8, hash_flag, 0);
+            else if (format[i] == 'x')
+                count += print_unsigned(va_arg(args, unsigned int), 16, hash_flag, 0);
+            else if (format[i] == 'X')
+                count += print_unsigned(va_arg(args, unsigned int), 16, hash_flag, 1);
+            else if (format[i] == '%')
+                count += _putchar('%');
             else
-                count += buffer_char(format[i], buffer, &buff_index);
+                count += _putchar(format[i]);
         }
         else
-            count += buffer_char(format[i], buffer, &buff_index);
+        {
+            count += _putchar(format[i]);
+        }
         i++;
     }
 
-    count += flush_buffer(buffer, &buff_index);
     va_end(args);
     return count;
+}
+
+/* Example main */
+int main(void)
+{
+    _printf("%+d\n", 42);        // +42
+    _printf("% d\n", 42);        //  42
+    _printf("%#o\n", 10);        // 012
+    _printf("%#x\n", 255);       // 0xff
+    _printf("%#X\n", 255);       // 0XFF
+    return 0;
 }
 
